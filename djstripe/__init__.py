4216bd926f6992e2e9b70251f1bc2266816ae963
Ruben <ruben@getfunky.nl>
2017-09-21 07:21:06 +0200

    allow run from source

--- djstripe/__init__.py
+++ djstripe/__init__.py
@@ -5,7 +5,10 @@
 """
 from __future__ import absolute_import, division, print_function, unicode_literals
 import pkg_resources
+
 from . import checks  # noqa: Register the checks
 
-
-__version__ = pkg_resources.require("dj-stripe")[0].version
+try:
+    __version__ = pkg_resources.require("dj-stripe")[0].version
+except pkg_resources.DistributionNotFound:
+    __version__ = 'no-dist'
